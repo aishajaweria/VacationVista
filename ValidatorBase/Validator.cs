718dd95f410036e0bb94c0894153a71b83ca53bd
aishajaweria <82979914+aishajaweria@users.noreply.github.com>
2023-12-27 19:31:50 +0500

    Add files via upload

---
+++ ValidatorBase/Validator.cs
@@ -0,0 +1,40 @@
+﻿using System.Text.RegularExpressions;
+
+
+namespace LeaveManagementSystem1._1
+{
+    public class ValidatingBase
+    {
+        //Method for validating email address
+        public static Regex Email_Address()
+        {
+            string Email_Pattern = @"^(?!\.)(""([^""\r\\]|\\[""\r\\])*""|"
+                + @"([-a-z0-9!#$%&'*+/=?^_`{|}~]|(?<!\.)\.)*)(?<!\.)"
+                + @"@[a-z0-9][\w\.-]*[a-z0-9]\.[a-z][a-z\.]*[a-z]$";
+
+            return new Regex(Email_Pattern, RegexOptions.IgnoreCase);
+        }
+
+        //Method for string validation only
+        public static Regex StringOnly()
+        {
+            string StringAndNumber_Pattern = "^[a-zA-Z]";
+
+            return new Regex(StringAndNumber_Pattern, RegexOptions.IgnoreCase);
+        }
+        //Method for numbers validation only
+        public static Regex NumbersOnly()
+        {
+            string StringAndNumber_Pattern = "^[0-9]*$";
+
+            return new Regex(StringAndNumber_Pattern, RegexOptions.IgnoreCase);
+        }
+        //Method for password validation only
+        public static Regex ValidPassword()
+        {
+            string Password_Pattern = "(?!^[0-9]*$)(?!^[a-zA-Z]*$)^([a-zA-Z0-9]{8,15})$";
+
+            return new Regex(Password_Pattern, RegexOptions.IgnoreCase);
+        }
+    }
+}
