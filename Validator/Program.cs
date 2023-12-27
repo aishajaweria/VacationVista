06c217e37f6088ca81154a2bfd61b7c96d7c1df5
aishajaweria <82979914+aishajaweria@users.noreply.github.com>
2023-12-27 19:32:42 +0500

    Add files via upload

---
+++ Validator/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.RegularExpressions;
+using System.Windows.Forms;
+using ValidatingBase;
+
+
+class Program
+{
+    private static object emailTextBox;
+
+    static void Main(string[] args)
+    {
+        ValidatingBase.ValidatingBase validator = new ValidatingBase.ValidatingBase();
+        if (emailTextBox.Text != null && !validator.Email_Address().IsMatch(emailTextBox.Text))
+        {
+            validator.emptymessage();
+        }
+
+    }
+}
