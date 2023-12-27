4aa4d7ad6d370b25de82d55db2a8cd99ca6d9502
aishajaweria <82979914+aishajaweria@users.noreply.github.com>
2023-12-27 18:52:20 +0500

    Add files via upload

---
+++ Viladation/ValidatingBase.cs
@@ -0,0 +1,50 @@
+﻿using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace ValidatingBase
+{
+    public class ValidatingBase
+    {
+        //Method for validating email address
+        public static Regex Email_Address()
+        {
+            string Email_Pattern = @"^(?!\.)(""([^""\r\\]|\\[""\r\\])*""|"
+                + @"([-a-z0-9!#$%&'*+/=?^_`{|}~]|(?<!\.)\.)*)(?<!\.)"
+                + @"@[a-z0-9][\w\.-]*[a-z0-9]\.[a-z][a-z\.]*[a-z]$";
+
+            return new Regex(Email_Pattern, RegexOptions.IgnoreCase);
+        }
+
+        //Method for string validation only
+        private static Regex StringOnly()
+        {
+            string StringAndNumber_Pattern = "^[a-zA-Z]";
+
+            return new Regex(StringAndNumber_Pattern, RegexOptions.IgnoreCase);
+        }
+        //Method for numbers validation only
+        private static Regex NumbersOnly()
+        {
+            string StringAndNumber_Pattern = "^[0-9]*$";
+
+            return new Regex(StringAndNumber_Pattern, RegexOptions.IgnoreCase);
+        }
+        //Method for password validation only
+        private static Regex ValidPassword()
+        {
+            string Password_Pattern = "(?!^[0-9]*$)(?!^[a-zA-Z]*$)^([a-zA-Z0-9]{8,15})$";
+
+            return new Regex(Password_Pattern, RegexOptions.IgnoreCase);
+        }
+
+        public void emptymessage()
+        {
+            MessageBox.Show("There are empty fields left that must be filled up!", "Invalid", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            return;
+        }
+        public void ResponsiveDtg(DataGridView dtg)
+        {
+            dtg.DefaultCellStyle.WrapMode = DataGridViewTriState.True;
+        }
+    }
+}
