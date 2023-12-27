8a09440c6c5b70704f811be9fce7c924e6ff9ba0
aishajaweria <82979914+aishajaweria@users.noreply.github.com>
2023-12-27 19:32:13 +0500

    Add files via upload

---
+++ Validator1/Program.cs
@@ -0,0 +1,24 @@
+﻿using LeaveManagementSystem1._1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//using ValidatingBase; // Assuming the ValidatingBase class is in the ValidatingBase namespace
+
+
+namespace Validator1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           // ValidatingBase.ValidatingBase validator = new ValidatingBase.ValidatingBase();
+            //if (!validator.Email_Address().IsMatch(emailTextBox.Text))
+            {
+               // validator.emptymessage();
+                // Handle invalid email
+            }
+        }
+    }
+}
